98fd8e5ac922e1f60791791a4981a0dc44510a40
rahulsingha7 <rahulsingha008@gmail.com>
2022-08-10 01:36:31 +0600

    layout

--- resources/views/admin/pages/login.blade.php
+++ resources/views/admin/pages/login.blade.php
@@ -1,10 +1,6 @@
-@extends('admin.layouts.default')
-@section('abcd')
+@extends('admin.layouts.temp')
 
-<div class="row justify-content-center">
-
-<div class="col-xl-10 col-lg-12 col-md-9">
-
+@section('temp')
     <div class="card o-hidden border-0 shadow-lg my-5">
         <div class="card-body p-0">
             <!-- Nested Row within Card Body -->
@@ -17,13 +13,12 @@
                         </div>
                         <form class="user">
                             <div class="form-group">
-                                <input type="email" class="form-control form-control-user"
-                                    id="exampleInputEmail" aria-describedby="emailHelp"
-                                    placeholder="Enter Email Address...">
+                                <input type="email" class="form-control form-control-user" id="exampleInputEmail"
+                                    aria-describedby="emailHelp" placeholder="Enter Email Address...">
                             </div>
                             <div class="form-group">
-                                <input type="password" class="form-control form-control-user"
-                                    id="exampleInputPassword" placeholder="Password">
+                                <input type="password" class="form-control form-control-user" id="exampleInputPassword"
+                                    placeholder="Password">
                             </div>
                             <div class="form-group">
                                 <div class="custom-control custom-checkbox small">
@@ -32,33 +27,27 @@
                                         Me</label>
                                 </div>
                             </div>
-                            <a href="{{url('dashboard')}}" class="btn btn-primary btn-user btn-block">
+                            <a href="index.html" class="btn btn-primary btn-user btn-block">
                                 Login
                             </a>
                             <hr>
-                            <a href="{{url('dashboard')}}" class="btn btn-google btn-user btn-block">
+                            <a href="index.html" class="btn btn-google btn-user btn-block">
                                 <i class="fab fa-google fa-fw"></i> Login with Google
                             </a>
-                            <a href="{{url('dashboard')}}" class="btn btn-facebook btn-user btn-block">
+                            <a href="index.html" class="btn btn-facebook btn-user btn-block">
                                 <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
                             </a>
                         </form>
                         <hr>
                         <div class="text-center">
-                            <a class="small" href="{{url('forgotpassword')}}">Forgot Password?</a>
+                            <a class="small" href="forgot-password.html">Forgot Password?</a>
                         </div>
                         <div class="text-center">
-                            <a class="small" href="{{url('register')}}">Create an Account!</a>
+                            <a class="small" href="register.html">Create an Account!</a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
-</div>
-
-</div>
- 
-
 @endsection
